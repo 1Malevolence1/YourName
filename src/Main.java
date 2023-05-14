7583db5e4416ea62898a3c2b2b00f872b0c17b1b
Mr.V <86906896+1Malevolence1@users.noreply.github.com>
2023-05-14 10:56:17 +0300

    Создана функция: "Генерация случайных букв"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
-        
+        int count = 0;
+        while (count < 10){
+            System.out.println(RandomLetter());
+            count +=1;
+        }
+    }
+
+    public static char RandomLetter(){
+        Random random = new Random();
+        String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+        int randomInt = random.nextInt(alphabet.length());
+        return alphabet.charAt(randomInt);
+
     }
 }
