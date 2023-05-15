c32a02e227f89ccd48672ab45f2c51ddba435c73
Mr.V <86906896+1Malevolence1@users.noreply.github.com>
2023-05-15 08:47:40 +0300

    основной код готов

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ввидите имя: ");
+        String user = scanner.nextLine();
+        String name = "";
         int count = 0;
-        while (count < 10){
-            System.out.println(RandomLetter());
-            count +=1;
+
+        while (!name.equals(user)) {
+            name = "";
+            for (int i = 0; i < user.length(); i++) {
+                char letter = RandomLetter();
+                name += letter;
+
+            }
+
+            if (name.equals(user)) {
+                System.out.println(name);
+                break;
+            }
+            System.out.println(name + ", " + count);
+            count++;
         }
+        System.out.println(name + ", " + count);
     }
 
-    public static char RandomLetter(){
+
+    public static char RandomLetter() {
         Random random = new Random();
         String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
